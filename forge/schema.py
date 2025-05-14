a34487e05be8ceed1a7e32eaa9d77132b6e42b08
brunolcarli <brunolcarli@gmail.com>
2025-05-14 19:23:22 -0300

    ADD base server

---
+++ forge/schema.py
@@ -0,0 +1,15 @@
+import graphene
+import graphql_jwt
+import api.schema
+
+
+class Query(api.schema.Query, graphene.ObjectType):
+    pass
+
+
+# class Mutation(api.schema.Mutation, graphene.ObjectType):
+#     validate_user_token = graphql_jwt.Verify.Field()
+#     refresh_user_token = graphql_jwt.Refresh.Field()
+
+
+schema = graphene.Schema(query=Query)
